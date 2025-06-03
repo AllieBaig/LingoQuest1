db6ad57319ef218842a6ffdf1ab7ea2f3200b836
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-06-03 09:50:47 +0100

    Update cinequest.js

--- js/modes/cinequest/cinequest.js
+++ js/modes/cinequest/cinequest.js
@@ -14,7 +14,7 @@ import {
   renderIngameHead,
   renderIngameFoot,
   showUserError
-} from '../../js/modeHelper.js';
+} from '../../modeHelper.js';
 
 let cineQuestions = [],
     currentIndex = 0;
