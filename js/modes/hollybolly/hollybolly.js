7c3eebc3679b5387ed4c7daf51b40a04093a1014
AllieBaig <136852901+AllieBaig@users.noreply.github.com>
2025-06-04 13:39:35 +0100

    Update hollybolly.js

--- js/modes/hollybolly/hollybolly.js
+++ js/modes/hollybolly/hollybolly.js
@@ -110,4 +110,15 @@ function getDifficulty() {
   return localStorage.getItem('game-difficulty') || 'medium';
 }
 
+/**
+ * Shuffles array in-place
+ */
+export function shuffleArray(array) {
+  if (!Array.isArray(array)) return [];
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
